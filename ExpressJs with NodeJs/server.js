00e05145b6e09b4ca38f06babe4341698070a265
batuhanbostanci <batuhanbostanci2@hotmail.com>
2023-06-05 22:21:23 +0300

    End of the nodemoon and basic understanding

--- ExpressJs with NodeJs/server.js
+++ ExpressJs with NodeJs/server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (request, response) {
-  response.send("<h1>Hello</h1>");
+app.get("/", function (req, res) {
+  res.send("<h1>Hello</h1>");
+});
+
+app.get("/contact", function (req, res) {
+  res.send("Contact me at: tacko@gmail.com");
+});
+
+app.get("/about", function (req, res) {
+  res.send("This is going to be learning path of me ");
+});
+
+app.get("/hobbies", function (req, res) {
+  res.send("<ul><li>Code</li><li>Coffe</li></ul>");
 });
 
 app.listen(3000, function () {
