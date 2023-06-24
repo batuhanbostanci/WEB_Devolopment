6f4e3018c6b1826f968e66208658f0a0ac92ac06
batuhanbostanci <batuhanbostanci2@hotmail.com>
2023-06-24 23:58:07 +0300

    Contiune ejs parts

--- Todolist-v1/app.js
+++ Todolist-v1/app.js
@@ -5,23 +5,41 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  var options = {
+  let options = {
     weekday: "long",
     year: "numeric",
     month: "long",
     day: "numeric",
   };
 
-  var day = new Date().toLocaleDateString("en-US", options);
+  let day = new Date().toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day });
+  res.render("list", { listTitle: day, newListItem: items });
 });
+
 app.post("/", function (req, res) {
-  res.send("Thanks for posting that.");
+  let item = req.body.newItem;
+  console.log(req.body);
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItem: workItems });
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
