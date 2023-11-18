00e465c35b6b31a4e51090433857e2b61ceae94f
batuhanbostanci <batuhanbostanci2@hotmail.com>
2023-11-19 00:36:43 +0300

    middle of the traveler project

--- PostgreDatabase/Travel Tracker/index.js
+++ PostgreDatabase/Travel Tracker/index.js
@@ -1,14 +1,52 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "postgre",
+  port: 5432,
+});
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+async function checkVisisted() {
+  let countries = [];
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+
+  return countries;
+}
+
 app.get("/", async (req, res) => {
-  //Write your code here.
+  const countries = await checkVisisted();
+  res.render("index.ejs", { countries: countries, total: countries.length });
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  );
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      data.country_code,
+    ]);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
