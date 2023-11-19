63b0c48083cbb7505f590ae9efac7d032b37217c
batuhanbostanci <batuhanbostanci2@hotmail.com>
2023-11-19 14:17:25 +0300

    correction of index.js for travel tracker

--- PostgreDatabase/Travel Tracker/index.js
+++ PostgreDatabase/Travel Tracker/index.js
@@ -38,7 +38,7 @@ app.post("/add", async (req, res) => {
 
   try {
     const result = await db.query(
-      "SELECT country_code FROM countries WHERE country_name = $1",
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
       [input]
     );
     const data = result.rows[0];
