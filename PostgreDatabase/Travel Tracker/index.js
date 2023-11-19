c7bdfc3284f221cbcf2c17fe6be25e2ee17f86e4
batuhanbostanci <batuhanbostanci2@hotmail.com>
2023-11-19 13:50:03 +0300

    End of the travel tracker project

--- PostgreDatabase/Travel Tracker/index.js
+++ PostgreDatabase/Travel Tracker/index.js
@@ -35,17 +35,36 @@ app.get("/", async (req, res) => {
 
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
-  const result = await db.query(
-    "SELECT country_code FROM countries WHERE country_name = $1",
-    [input]
-  );
 
-  if (result.rows.length !== 0) {
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE country_name = $1",
+      [input]
+    );
     const data = result.rows[0];
-    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
-      data.country_code,
-    ]);
-    res.redirect("/");
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [data.country_code]
+      );
+      res.redirect("/");
+    } catch (error) {
+      console.log(error);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
   }
 });
 
