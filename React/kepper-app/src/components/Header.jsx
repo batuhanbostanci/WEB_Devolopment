09b285be3d378e2ee048f26cabe46b8974205652
batuhanbostanci <batuhanbostanci2@hotmail.com>
2023-11-25 16:31:48 +0300

    Starting React

---
+++ React/kepper-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// import Header from "./Header";
+function Heading() {
+  return (
+    <header>
+      <h1>Keeper</h1>
+    </header>
+  );
+}
+
+export default Heading;
---
+++ React/kepper-app/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./components/Header";
+import Footer from "./components/Footer";
+import App from "./components/App";
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
