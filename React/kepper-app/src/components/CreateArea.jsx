361b2fb6308979474f076f26e882367544761733
batuhanbostanci <batuhanbostanci2@hotmail.com>
2023-12-01 22:17:17 +0300

    end of the keeper app

--- React/kepper-app/src/components/CreateArea.jsx
+++ React/kepper-app/src/components/CreateArea.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handelChange(event) {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function sumbitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form className="create-note">
+        <input
+          name="title"
+          onChange={handelChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handelChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={sumbitNote}>Add</button>
       </form>
     </div>
   );
