333b7657d2defc94122f1261ad02c20c6d4989d2
batuhanbostanci <batuhanbostanci2@hotmail.com>
2023-12-10 14:08:39 +0300

    end of the fast pizza part

--- FullyReact/03-pizza-menu/src/index.js
+++ FullyReact/03-pizza-menu/src/index.js
@@ -1,17 +1,124 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+function App() {
+  return (
+    <div className="container">
+      <h1>React Pizza Menu</h1>
+    </div>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Headers() {
+  const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  return (
+    <header className="header">
+      <div>
+        <h1>Fast Tacko Pizza Co.</h1>
+      </div>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <div className="menu">
+      <h2>Tacko Menu</h2>
+      {pizzaData.map((item) => {
+        return (
+          <Pizza
+            name={item.name}
+            ingredients={item.ingredients}
+            photoName={item.photoName}
+            price={item.price}
+          />
+        );
+      })}
+    </div>
+  );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  //   if (hour >= openHour && hour <= closeHour) alert("We're currently open");
+  //   else alert("Soory we're closed");
+  //   console.log(hour);
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}We're currently open
+    </footer>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} width="200" height="200" />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{parseInt(props.price) + 5}</span>
+      </div>
+    </div>
+  );
+}
+
+//React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Headers />
     <App />
+    <Menu />
+    <Footer />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
