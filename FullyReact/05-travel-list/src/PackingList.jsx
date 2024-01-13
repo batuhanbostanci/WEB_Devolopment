212f4b933b9f5c67f43badff1209667d42ac1469
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-01-13 21:20:21 +0300

    Adding sorting filters

--- FullyReact/05-travel-list/src/PackingList.jsx
+++ FullyReact/05-travel-list/src/PackingList.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 function PackingList({ items, onDeleteItems, onToogleItems }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  else if (sortBy === "description")
+    sortedItems = [...items].sort((a, b) =>
+      a.description.localeCompare(b.description)
+    );
+  else if (sortBy === "packed")
+    sortedItems = [...items].sort((a, b) => a.packed - b.packed);
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => {
+        {sortedItems.map((item) => {
           return (
             <Item
               item={item}
@@ -16,6 +28,14 @@ function PackingList({ items, onDeleteItems, onToogleItems }) {
           );
         })}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
