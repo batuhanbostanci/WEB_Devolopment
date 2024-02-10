6f67af222078e87db65ca70459134bed434c3267
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-02-10 15:47:02 +0300

    Adding useRef use-case

--- FullyReact/07-usepopcorn/src/App.js
+++ FullyReact/07-usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, userRef, useEffect, useRef } from "react";
 import "./index.css";
 import StarRating from "./StarRating";
 
@@ -146,6 +146,15 @@ function MovieDetails({ selectedId, onCloseMovie, onhandelAddWatch, watched }) {
   const [loading, setLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
 
+  const countRef = useRef(0);
+
+  useEffect(
+    function () {
+      if (userRating) countRef.current = countRef.current + 1;
+    },
+    [userRating]
+  );
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId
@@ -173,6 +182,7 @@ function MovieDetails({ selectedId, onCloseMovie, onhandelAddWatch, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     };
 
     onhandelAddWatch(newWatchedMovie);
@@ -327,6 +337,31 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      //console.log(inputEl.current);
+
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === "Enter") inputEl.current.focus();
+        setQuery("");
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return () => document.removeEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
+  // useEffect(function () {
+  //   const el = document.querySelector(".search");
+  //   console.log(el);
+  //   el.focus();
+  // }, []);
+
   return (
     <input
       className="search"
@@ -334,6 +369,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
