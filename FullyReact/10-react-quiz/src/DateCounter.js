1ace29ed873e37b9d51a35e824f8c4c732622b29
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-02-11 15:05:43 +0300

    End of the first introduction of useReducer

--- FullyReact/10-react-quiz/src/DateCounter.js
+++ FullyReact/10-react-quiz/src/DateCounter.js
@@ -1,34 +1,56 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  console.log(state, action);
+  if (action.type === "inc") {
+    return state + 1;
+  }
+  if (action.type === "dec") {
+    return state - 1;
+  }
+  if (action.type === "setCount") {
+    return action.payload;
+  }
+  if (action.type === "reset") {
+    return 0;
+  }
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
+
+  const [count, dispatch] = useReducer(reducer, 0);
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
+    dispatch({ type: "dec" });
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    //setCount((count) => count - step);
   };
 
   const inc = function () {
+    dispatch({ type: "inc" });
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    //setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    //setStep(Number(e.target.value));
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
     setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
