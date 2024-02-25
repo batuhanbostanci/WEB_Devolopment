e30d49bdf7da03f23d2ea92c421825f2e92b1b7d
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-02-25 16:59:31 +0300

    Creating Custom Provider

--- FullyReact/11-worldwise/src/App.jsx
+++ FullyReact/11-worldwise/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -10,62 +9,36 @@ import CityList from "./components/CityList";
 import CountriesList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-
-const BASE_URL = "http://localhost:8000";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch (error) {
-        alert("Error fetching cities");
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          {/* Routes go here */}
-          <Route index element={<Homepage />}></Route>
-          <Route path="product" element={<Product />}></Route>
-          <Route path="pricing" element={<Pricing />}></Route>
-          <Route path="login" element={<Login />}></Route>
-          <Route path="app" element={<AppLayout />}>
-            <Route
-              index
-              element={<Navigate to="cities" replace></Navigate>}
-            ></Route>
-            <Route
-              path="cities"
-              element={<CityList cities={cities} loading={loading}></CityList>}
-            ></Route>
-            <Route path="cities/:id" element={<City></City>}></Route>
-            <Route
-              path="countries"
-              element={
-                <CountriesList
-                  cities={cities}
-                  loading={loading}
-                ></CountriesList>
-              }
-            ></Route>
-            <Route path="form" element={<Form></Form>}></Route>
-          </Route>
-          <Route path="*" element={<PageNotFound />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* Routes go here */}
+            <Route index element={<Homepage />}></Route>
+            <Route path="product" element={<Product />}></Route>
+            <Route path="pricing" element={<Pricing />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="app" element={<AppLayout />}>
+              <Route
+                index
+                element={<Navigate to="cities" replace></Navigate>}
+              ></Route>
+              <Route path="cities" element={<CityList></CityList>}></Route>
+              <Route path="cities/:id" element={<City></City>}></Route>
+              <Route
+                path="countries"
+                element={<CountriesList></CountriesList>}
+              ></Route>
+              <Route path="form" element={<Form></Form>}></Route>
+            </Route>
+            <Route path="*" element={<PageNotFound />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
     </div>
   );
 }
---
+++ FullyReact/11-worldwise/src/contexts/CitiesContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useEffect, useState } from "react";
+
+const CitiesContext = createContext();
+
+const BASE_URL = "http://localhost:8000";
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert("Error fetching cities");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, loading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export { CitiesContext, CitiesProvider };
