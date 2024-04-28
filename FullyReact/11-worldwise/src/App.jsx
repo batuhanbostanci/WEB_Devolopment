d65ae00f8463910eeeca253f60b9588f67d1212d
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-04-28 14:26:21 +0300

    Adding lazy function for code spliting. End of the project

--- FullyReact/11-worldwise/src/App.jsx
+++ FullyReact/11-worldwise/src/App.jsx
@@ -1,51 +1,66 @@
+import { lazy } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Homepage from "./pages/Homepage";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
-import Login from "./pages/Login";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
 import CityList from "./components/CityList";
 import CountriesList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
+import { Suspense } from "react";
+
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Homepage from "./pages/Homepage";
+// import PageNotFound from "./pages/PageNotFound";
+// import AppLayout from "./pages/AppLayout";
+// import Login from "./pages/Login";
+
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Login = lazy(() => import("./pages/Login"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
-          <Routes>
-            {/* Routes go here */}
-            <Route index element={<Homepage />}></Route>
-            <Route path="product" element={<Product />}></Route>
-            <Route path="pricing" element={<Pricing />}></Route>
-            <Route path="login" element={<Login />}></Route>
-            <Route
-              path="app"
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              {/* Routes go here */}
+              <Route index element={<Homepage />}></Route>
+              <Route path="product" element={<Product />}></Route>
+              <Route path="pricing" element={<Pricing />}></Route>
+              <Route path="login" element={<Login />}></Route>
               <Route
-                index
-                element={<Navigate to="cities" replace></Navigate>}
-              ></Route>
-              <Route path="cities" element={<CityList></CityList>}></Route>
-              <Route path="cities/:id" element={<City></City>}></Route>
-              <Route
-                path="countries"
-                element={<CountriesList></CountriesList>}
-              ></Route>
-              <Route path="form" element={<Form></Form>}></Route>
-            </Route>
-            <Route path="*" element={<PageNotFound />}></Route>
-          </Routes>
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route
+                  index
+                  element={<Navigate to="cities" replace></Navigate>}
+                ></Route>
+                <Route path="cities" element={<CityList></CityList>}></Route>
+                <Route path="cities/:id" element={<City></City>}></Route>
+                <Route
+                  path="countries"
+                  element={<CountriesList></CountriesList>}
+                ></Route>
+                <Route path="form" element={<Form></Form>}></Route>
+              </Route>
+              <Route path="*" element={<PageNotFound />}></Route>
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
