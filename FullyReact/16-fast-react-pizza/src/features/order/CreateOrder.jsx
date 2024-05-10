3c623c883079962a1c60b52524dac66fcdde1a5d
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-05-10 21:07:38 +0300

    adding error handling in form actions

--- FullyReact/16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ FullyReact/16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 // https://uibakery.io/regex-library/phone-number
 
 const isValidPhone = (str) =>
-  "/^+?d{1,4}?[-.s]?(?d{1,3}?)?[-.s]?d{1,4}[-.s]?d{1,4}[-.s]?d{1,9}$/".test(
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str
   );
 
@@ -33,6 +33,10 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigation = useNavigation();
+  const isSubmitting = (navigation.state = "submitting");
+
+  const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -52,6 +56,8 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -74,7 +80,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button>{isSubmitting ? "Packing order...." : "Order now"}</button>
         </div>
       </Form>
     </div>
@@ -91,6 +97,14 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart),
     priority: data.priority === "on",
   };
+
+  const errors = {};
+
+  if (!isValidPhone(order.phone)) errors.phone = "Invalid phone number";
+
+  if (Object.keys(errors).length > 0) {
+    return errors;
+  }
 
   const newOrder = await createOrder(order);
   return redirect(`/order/${newOrder.id}`);
