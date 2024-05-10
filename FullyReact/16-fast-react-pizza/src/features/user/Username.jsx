59968b9238566490a53a08ce341b139984bf1823
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-05-10 22:42:23 +0300

    Adding spacing borders display properties to the project

--- FullyReact/16-fast-react-pizza/src/features/user/Username.jsx
+++ FullyReact/16-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Batu Software </div>;
+  return <div className=" hidden text-sm font-semibold">Batu Software </div>;
 }
 
 export default Username;
