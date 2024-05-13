817e5060d8e16bbc0f4d3ecff85cc39ca98e46af
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-05-13 21:32:17 +0300

    End of the styling cart

--- FullyReact/16-fast-react-pizza/src/ui/Button.jsx
+++ FullyReact/16-fast-react-pizza/src/ui/Button.jsx
@@ -6,6 +6,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + 'md:px-6 md:py-4 px-4 py-3',
     small: base + 'px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    secondary:
+      'text-stone-400 uppercase-800 md:px-6 md:py-3 px-4 py-2 trans transition-colors-300 focus-ring rounded-full border-2 border-stone-300 hover: text-stone-800 font-semibold tracking-wide duration-300 hover:bg-stone-300 focus:outline-none focus:ring-stone-200 focus:ring-offset-2 focus:text-stone-400 disabled:cursor-not-allowed',
   };
   if (to) {
     return (
