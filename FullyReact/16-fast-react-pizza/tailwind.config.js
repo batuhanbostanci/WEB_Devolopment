9dacd8f42434ac8975fa0a2f57b5a1e955e5babf
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-05-13 19:57:54 +0300

    Adding custom font familiy and extend tailwind config

--- FullyReact/16-fast-react-pizza/tailwind.config.js
+++ FullyReact/16-fast-react-pizza/tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+//eslint-disable-next-line
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: { pizza: 'Roboto Mono, monospace' },
+    extend: {
+      /* If we put that colors to the theme part,
+    we can not use tailwind colors. Instead of putting themes part, if we put 
+    colors to the extend part. We can extend our colors.*/
+      colors: {
+        pizza: '#12345',
+      },
+      fontSize: {
+        huge: '40rem, { lineHeight: 1 }',
+      },
+
+      //This is important for the mobile devices. It will take the full height of the screen.
+      height: { screen: '100dvh' },
+    },
   },
   plugins: [],
 };
