97a1f06416d869e68f04695e521ccd14808f0f95
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-05-15 23:23:13 +0300

    Adding Cart Selectors to the app

--- FullyReact/16-fast-react-pizza/src/features/cart/cartSlice.js
+++ FullyReact/16-fast-react-pizza/src/features/cart/cartSlice.js
@@ -1,16 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  //cart: [],
-  cart: [
-    {
-      id: 1,
-      name: 'Pizza Margherita',
-      unitPrice: 10,
-      quantity: 3,
-      totalPrice: 30,
-    },
-  ],
+  cart: [],
+  //   cart: [
+  //     {
+  //       id: 1,
+  //       name: 'Pizza Margherita',
+  //       unitPrice: 10,
+  //       quantity: 3,
+  //       totalPrice: 30,
+  //     },
+  //   ],
 };
 
 const cartSlice = createSlice({
@@ -51,3 +51,12 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+//Those are can cause performance issues in redux toolkit for that reason we need to
+//fouse "reselect"
