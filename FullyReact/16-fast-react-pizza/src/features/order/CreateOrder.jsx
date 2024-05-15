456f31709c756969493c359ac987eb10718ff6de
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-05-15 22:22:27 +0300

    reading and updating user state

--- FullyReact/16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ FullyReact/16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 // https://uibakery.io/regex-library/phone-number
 
 const isValidPhone = (str) =>
@@ -36,6 +37,7 @@ const fakeCart = [
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = (navigation.state = 'submitting');
+  const username = useSelector((state) => state.user.username);
 
   const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
@@ -50,7 +52,13 @@ function CreateOrder() {
         <div className="mb-5 flex flex-col items-center gap-2 sm:flex-row">
           <label className="sm:basis-40">First Name</label>
           {/*Ussing this class name input should be the exception because making everything in the index.css will be the oldway of doing that for the learning path input would implement here!*/}
-          <input type="text" name="customer" required className="input grow" />
+          <input
+            type="text"
+            name="customer"
+            defaultValue={username}
+            required
+            className="input grow"
+          />
         </div>
 
         <div className="mb-5 flex flex-col items-center gap-2 sm:flex-row">
