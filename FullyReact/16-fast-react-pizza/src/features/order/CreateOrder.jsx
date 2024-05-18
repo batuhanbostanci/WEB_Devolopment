3fe48d45139264f1ea1a5247b8b5b007432e3bfd
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-05-18 12:06:59 +0300

    End of the cart for new orders part

--- FullyReact/16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ FullyReact/16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -3,6 +3,10 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
 import { useSelector } from 'react-redux';
+import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
+import EmptyCart from '../cart/EmptyCart';
+import store from '../../store';
+import { formatCurrency } from '../../utils/helpers';
 // https://uibakery.io/regex-library/phone-number
 
 const isValidPhone = (str) =>
@@ -10,38 +14,20 @@ const isValidPhone = (str) =>
     str,
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function CreateOrder() {
+  const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
-  const isSubmitting = (navigation.state = 'submitting');
+  const isSubmitting = navigation.state === 'submitting';
+  console.log(isSubmitting);
   const username = useSelector((state) => state.user.username);
 
   const formErrors = useActionData();
-  // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
+
+  const totalPrice = totalCartPrice + priorityPrice;
+  if (!cart.length) return <EmptyCart></EmptyCart>;
 
   return (
     <div className="px-4 py-6">
@@ -91,8 +77,8 @@ function CreateOrder() {
             name="priority"
             id="priority"
             className="h-6 w-6 accent-yellow-400 duration-300  focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 "
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label className="font-medium" htmlFor="priority">
             Want to yo give your order priority?
@@ -102,7 +88,9 @@ function CreateOrder() {
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button type="primary">
-            {isSubmitting ? 'Packing order....' : 'Order now'}
+            {isSubmitting
+              ? 'Placing order....'
+              : `Order now from ${formatCurrency(totalPrice)}€`}
           </Button>
         </div>
       </Form>
@@ -118,7 +106,7 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === 'on',
+    priority: data.priority === 'true',
   };
 
   const errors = {};
@@ -129,10 +117,13 @@ export async function action({ request }) {
     return errors;
   }
 
-  // const newOrder = await createOrder(order);
-  // return redirect(`/order/${newOrder.id}`);
+  const newOrder = await createOrder(order);
 
-  return null;
+  //Normally we use useDispatch but this is used only in components for this reason we use store in the function directly import the store.js file
+  // Do not overuse this techinque because it's not a good practice and it will be cause some optimization problems with redux
+  store.dispatch(clearCart());
+
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
--- FullyReact/16-fast-react-pizza/src/features/order/Order.jsx
+++ FullyReact/16-fast-react-pizza/src/features/order/Order.jsx
@@ -53,7 +53,7 @@ function Order() {
       </div>
       <ul className="divide-y divide-stone-200 border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.id}></OrderItem>
+          <OrderItem item={item} key={item.pizzaId}></OrderItem>
         ))}
       </ul>
       <div className="space-y-2 bg-stone-200 px-6 py-5">
