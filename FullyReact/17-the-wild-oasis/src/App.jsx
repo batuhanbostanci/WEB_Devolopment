267bffccf7baa67ddde9bac3db6e6e30a8714af3
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-05-19 12:54:34 +0300

    End of the reusable components

--- FullyReact/17-the-wild-oasis/src/App.jsx
+++ FullyReact/17-the-wild-oasis/src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -14,12 +15,31 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert("Check in")}>Click me!</Button>
-        <Button onClick={() => alert("Check out")}>Click out!</Button>
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="number of guests"></Input>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Check in")}>Click me!</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Check out")}
+              >
+                Click out!
+              </Button>
+            </div>
+          </Row>
+          <Heading as="h3">Form</Heading>
+
+          <Row type="vertical">
+            <form>
+              <Input type="number" placeholder="number of guests"></Input>
+              <Input type="number" placeholder="number of guests"></Input>
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
