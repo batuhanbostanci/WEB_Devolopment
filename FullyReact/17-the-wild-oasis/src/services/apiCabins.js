363bd3ac67638c11412a74bc462e313d356c92f6
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-05-25 16:12:49 +0300

    Deleting cabins with mutasions

--- FullyReact/17-the-wild-oasis/src/services/apiCabins.js
+++ FullyReact/17-the-wild-oasis/src/services/apiCabins.js
@@ -4,8 +4,19 @@ export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
-    console.log("Cabins could not be loaded");
-    throw new Error(error.message);
+    console.log(error);
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.log(error);
+    throw new Error("Cabins could not be deleted");
   }
 
   return data;
