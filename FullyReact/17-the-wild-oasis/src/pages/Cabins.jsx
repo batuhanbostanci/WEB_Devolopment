0bce1e6ce2e9751df10ff0ab54d0d9006a1e3c13
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-05-26 13:15:27 +0300

    Starting the react hook form

--- FullyReact/17-the-wild-oasis/src/pages/Cabins.jsx
+++ FullyReact/17-the-wild-oasis/src/pages/Cabins.jsx
@@ -1,10 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
-
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -14,6 +16,12 @@ function Cabins() {
 
       <Row>
         <CabinTable></CabinTable>
+
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add New Cabin
+        </Button>
+
+        {showForm && <CreateCabinForm></CreateCabinForm>}
       </Row>
     </>
   );
