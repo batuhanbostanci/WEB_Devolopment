24315c90678e07601f402de215eb602bf76bc322
batuhanbostanci <batuhanbostanci2@hotmail.com>
2025-01-06 11:16:17 +0300

    End of the signup form page

--- FullyReact/17-the-wild-oasis/src/ui/Input.jsx
+++ FullyReact/17-the-wild-oasis/src/ui/Input.jsx
@@ -1,9 +1,9 @@
 import styled, { css } from "styled-components";
 
 const Input = styled.input`
-  border: 1px solid var(--color-gray-300);
+  border: 1px solid var(--color-gray-700);
   box-shadow: var(--shadow-sm);
-  background-color: var(--color-gray-0);
+  background-color: var(--color-gray-700);
   padding: 0.8rem 1.2rem;
   border-radius: var(--border-radius-sm);
 `;
