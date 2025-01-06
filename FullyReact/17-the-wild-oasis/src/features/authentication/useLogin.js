7f751d63cb33b9f00d83988144fbf2af97c85675
batuhanbostanci <batuhanbostanci2@hotmail.com>
2025-01-06 09:06:51 +0300

    End of the user logout from application

--- FullyReact/17-the-wild-oasis/src/features/authentication/useLogin.js
+++ FullyReact/17-the-wild-oasis/src/features/authentication/useLogin.js
@@ -11,7 +11,7 @@ export function useLogin() {
 
     onSuccess: (user) => {
       queryClient.setQueryData(["user"], () => user.user);
-      navigate("/dashboard");
+      navigate("/dashboard", { replace: true });
     },
 
     onError: (err) => {
