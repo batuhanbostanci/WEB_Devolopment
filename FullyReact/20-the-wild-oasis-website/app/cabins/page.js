36c761eb4b6cce6c053917162c998242ebdf2399
batuhanbostanci <batuhanbostanci2@hotmail.com>
2025-01-10 22:54:15 +0300

    End of the fetching data in the page

--- FullyReact/20-the-wild-oasis-website/app/cabins/page.js
+++ FullyReact/20-the-wild-oasis-website/app/cabins/page.js
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <h1>Cabins Page</h1>;
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
+  return (
+    <div>
+      <h1>Cabins Page</h1>
+      <div>
+        <ul>
+          {data.map((user) => (
+            <li key={user.id}>{[user.name]}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
