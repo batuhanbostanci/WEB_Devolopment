2f68fc2a75849a6ba730b575f44e551342a1c40e
batuhanbostanci <batuhanbostanci2@hotmail.com>
2025-01-14 21:40:15 +0300

    End of the generating dynamic metadata

--- FullyReact/20-the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ FullyReact/20-the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -1,5 +1,16 @@
+import Image from "next/image";
 import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
+
+// export const metadata = {
+//   title: "Cabin",
+// };
+
+export async function generateMetadata({ params }) {
+  const { name } = await getCabin(params.cabinId);
+
+  return { title: `Cabin ${name}` };
+}
 
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
