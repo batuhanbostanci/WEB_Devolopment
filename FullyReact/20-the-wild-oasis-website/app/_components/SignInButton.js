72a7683596c809c183e45ce163791bc9343264b5
batuhanbostanci <batuhanbostanci2@hotmail.com>
2025-01-22 08:01:55 +0300

    End of the building custom signin page

--- FullyReact/20-the-wild-oasis-website/app/_components/SignInButton.js
+++ FullyReact/20-the-wild-oasis-website/app/_components/SignInButton.js
@@ -1,14 +1,20 @@
+import { signInAction } from "../_lib/actions";
+
 function SignInButton() {
   return (
-    <button className='flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium'>
-      <img
-        src='https://authjs.dev/img/providers/google.svg'
-        alt='Google logo'
-        height='24'
-        width='24'
-      />
-      <span>Continue with Google</span>
-    </button>
+    // This is going to be a server side form, that is why we get the button inside a form, then we can use its
+    // action attribute to send the form data to the server.
+    <form action={signInAction}>
+      <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
+        <img
+          src="https://authjs.dev/img/providers/google.svg"
+          alt="Google logo"
+          height="24"
+          width="24"
+        />
+        <span>Continue with Google</span>
+      </button>
+    </form>
   );
 }
 
--- FullyReact/20-the-wild-oasis-website/app/account/page.js
+++ FullyReact/20-the-wild-oasis-website/app/account/page.js
@@ -1,7 +1,12 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest Area",
 };
 
-export default function Page() {
-  return <h1>Welcome, Jonas</h1>;
+export default async function Page() {
+  const session = await auth();
+
+  const firstName = session.user.name.split(" ")[0];
+  return <h1>Welcome, {firstName}</h1>;
 }
