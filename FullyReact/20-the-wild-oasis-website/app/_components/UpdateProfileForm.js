7c4397e2394541efdc5e4e0dc88b18473fbd5d28
batuhanbostanci <batuhanbostanci2@hotmail.com>
2025-02-05 12:38:06 +0300

    End of the updating reservation part

--- FullyReact/20-the-wild-oasis-website/app/_components/UpdateProfileForm.js
+++ FullyReact/20-the-wild-oasis-website/app/_components/UpdateProfileForm.js
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { updateGuest } from "../_lib/actions";
 import { useFormStatus } from "react-dom";
+import SubmitButton from "./SumbitButton";
 
 function UpdateProfileForm({ children, guest }) {
   const [count, setCount] = useState();
@@ -56,22 +57,9 @@ function UpdateProfileForm({ children, guest }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <Button />
+        <SubmitButton pendingLabel="Updating...">Update Profile</SubmitButton>
       </div>
     </form>
-  );
-}
-//UseFormStatus should be use like that.
-function Button() {
-  const { pending } = useFormStatus();
-
-  return (
-    <button
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all isabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-      disabled={pending}
-    >
-      {pending ? "Updating..." : "Update profile"}
-    </button>
   );
 }
 
